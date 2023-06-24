c0d6225bb77e13286a23ff9ad3d862cb8bdc094a
Lucas <molinag.lucas@gmail.com>
2023-06-24 15:22:55 -0700

    Add name column to items table

--- db/schema/03_items.sql
+++ db/schema/03_items.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS items CASCADE;
 
 CREATE TABLE items (
   id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
   price FLOAT NOT NULL,
   picture_url TEXT,
   description TEXT,
