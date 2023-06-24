751e1ca3ea50bd3e16b9da353bc0d1c7f1424ef7
Sethu Nair <sethunair2102@gmail.com>
2023-06-24 16:11:06 -0600

    menu ejs started

---
+++ public/scripts/menu.js
@@ -0,0 +1,8 @@
+const menutItem = function(item) {`
+<section class="menu-item">
+    <img src="${item.picture_url}" alt=" pasta bake"/>
+    <p class ="item-description">${item.description}</p>
+    <p class="price"> $${item.price}</p>
+    <button class="add-to-order">Add</button>
+  </div>
+`}
