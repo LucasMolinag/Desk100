13fc824187139d6b09a2b59d02c214a37028e896
Sethu Nair <sethunair2102@gmail.com>
2023-06-23 19:41:35 -0600

    schema-items

---
+++ db/schema/03_items.sql
@@ -0,0 +1,11 @@
+-- Drop and recreate Users table (Example)
+
+DROP TABLE IF EXISTS items CASCADE;
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  price FLOAT NOT NULL,
+  picture_url TEXT,
+  description TEXT,
+  cook_time_in_minutes INT
+);
