4dff658daf29298de28c0d2033fd929bf52e44a7
Lucas <molinag.lucas@gmail.com>
2023-06-27 19:29:58 -0700

    Implement getOrderItems

---
+++ db/queries/04_get_order_items.js
@@ -0,0 +1,16 @@
+const db = require('../connection');
+
+const getOrderItems = function () 
+{
+  return db
+  .query(
+    'SELECT items.name, items.price, order_items.quantity FROM items JOIN order_items ON order_items.item_id = items.id JOIN orders ON orders.id = order_items.order.id WHERE item_id = $1 WHERE order_id = $2 GROUP BY items.id, order_items.quantity ORDER BY items.id;', [ID])
+    .then(data => {
+      return data.rows;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+};
+
+module.exports = { getOrderItems };
